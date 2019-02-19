052c498e42a218e94ef9c699e7e987a3e1ccd798
Aleksandr Ivanishko <ivanishco@mail.ru>
2019-02-19 23:15:47 +0300

    added counter

--- www/wp-content/themes/titletheme/footer.php
+++ www/wp-content/themes/titletheme/footer.php
@@ -60,7 +60,29 @@
 	<div class="zerogrid wrapper">
 		Copyright @ 2011-<?php echo date('Y');?> - <a href="http://www.megacode-it.ru" target="_blank" rel="nofollow">Проект Megacode-IT</a>
 		<ul class="quick-link">
-			<li><a href="#">Ссылка</a></li>
+			<li><a href="#">Ссылка</a>
+                <!-- Yandex.Metrika informer -->
+                <a href="https://metrika.yandex.ru/stat/?id=6307444&amp;from=informer"
+                   target="_blank" rel="nofollow"><img src="https://informer.yandex.ru/informer/6307444/3_1_FFFFFFFF_EFEFEFFF_0_pageviews"
+                                                       style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" class="ym-advanced-informer" data-cid="6307444" data-lang="ru" /></a>
+                <!-- /Yandex.Metrika informer -->
+
+                <!-- Yandex.Metrika counter -->
+                <script type="text/javascript" >
+                    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                        m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+                    ym(6307444, "init", {
+                        id:6307444,
+                        clickmap:true,
+                        trackLinks:true,
+                        accurateTrackBounce:true
+                    });
+                </script>
+                <noscript><div><img src="https://mc.yandex.ru/watch/6307444" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+                <!-- /Yandex.Metrika counter -->
+            </li>
 		</ul>
 	</div>
 </div>
